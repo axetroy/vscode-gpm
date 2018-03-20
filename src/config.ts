616852ad98a2e5b4aaed42132db4d0acd580f0e3
axetroy <troy450409405@gmail.com>
2018-03-20 15:29:43 +0800

    feat: support env in rootPath

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,11 @@ function getField(field: string): any {
 }
 
 function getRootPath(): string {
-  return getField("rootPath").replace(/^~/, process.env.HOME as string);
+  return getField("rootPath")
+    .replace(/^~/, process.env.HOME as string)
+    .replace(/\$\w+/, (word: string) => {
+      return process.env[word.replace(/^\$/, "")];
+    });
 }
 
 export { getConfiguration, getField, getRootPath };
