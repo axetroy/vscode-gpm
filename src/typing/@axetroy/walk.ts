f9056ceb3b1b320a4a26027c84fa242b25511f59
axetroy <troy450409405@gmail.com>
2018-07-31 09:30:38 +0800

    docs: update typing

--- src/typing/@axetroy/walk.ts
+++ src/typing/@axetroy/walk.ts
@@ -1,14 +1,18 @@
 declare module "@axetroy/walk" {
-  type Event = "file" | "directory" | "error";
+  import { Stats } from "fs";
 
-  type callback = (filepath: string) => void;
+  type fileCallback = (filepath: string, stat: Stats) => void;
+  type errorCallback = (error: Error) => void;
+  type doneCallback = () => void;
 
   interface IWalker {
     new (rootPath: string): Walker;
   }
 
   class Walker {
-    public on(event: Event, cb: callback): Walker;
+    public on(event: "file" | "directory", cb: fileCallback): Walker;
+    public on(event: "error", cb: errorCallback): Walker;
+    public on(event: "done", cb: doneCallback): Walker;
     public walk(): Promise<void>;
   }
 
